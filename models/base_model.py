f30595cc7d03a5adcd22b489e91c00ba13b46d04
Mawia <116746672+mkatiwa@users.noreply.github.com>
2023-09-16 21:40:59 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,46 @@
 #!/usr/bin/python3
-"""Defines BaseModel class."""
-import models
-from uuid import uuid4
+""" Module for Base """
+
 from datetime import datetime
+from uuid import uuid4
+import json
+import models
+
+format_date = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """
-        Class Base
-    """
+    """ Basemodel class """
 
     def __init__(self, *args, **kwargs):
-        """Initialize new BaseModel."""
-
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        """ Initialization of Database """
+        if args is not None and len(args) > 0:
+            pass
         if kwargs:
-            kwargs["created_at"] = datetime.strptime(
-                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
+            for key, item in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    item = datetime.strptime(item, format_date)
+                if key not in ['__class__']:
+                    setattr(self, key, item)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
+    def __str__(self):
+        """str definition"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
     def save(self):
-        """Set updated_at with current datetime."""
+        """save definition"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Return dictionary of BaseModel instance.
-        """
-        ndict = self.__dict__.copy()
-        ndict["created_at"] = self.created_at.isoformat()
-        ndict["updated_at"] = self.updated_at.isoformat()
-        ndict["__class__"] = self.__class__.__name__
-        return ndict
-
-    def __str__(self):
-        """Return print/str representation of BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
