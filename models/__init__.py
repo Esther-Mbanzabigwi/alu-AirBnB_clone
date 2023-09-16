1bb38e0b09d7a3d57d21edd95200db0e3f3e0504
Mawia <116746672+mkatiwa@users.noreply.github.com>
2023-09-16 21:37:36 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-""" for models directory"""
-from models.engine.file_storage import FileStorage
+#from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.engine import file_storage
 
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
